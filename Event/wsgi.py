dda590a4bc0490708dd305d09175b3b452387022
Dev Shakeywar <122546955+dev9310@users.noreply.github.com>
2025-02-09 15:13:37 +0530

    commit

--- Event/wsgi.py
+++ Event/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event.settings')
 
-app = get_wsgi_application()  # This should be defined
+application = get_wsgi_application()  # Ensure this line exists
