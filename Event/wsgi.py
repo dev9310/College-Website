d613131f6e7f1bfb16cf8d86ca56b3d33971d972
Dev Shakeywar <122546955+dev9310@users.noreply.github.com>
2025-02-09 15:18:55 +0530

    Update wsgi.py

--- Event/wsgi.py
+++ Event/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event.settings')
 
-application = get_wsgi_application()  # Ensure this line exists
+app = get_wsgi_application()  # ✅ Vercel requires `app`, not just `application`
